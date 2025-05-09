c882269544e93346a45fbdaf3d45b462e0125680
Saul Muñoz <58341096+Not-Minimal@users.noreply.github.com>
2025-05-09 01:03:09 -0400

    Se suben dotfiles de nvim

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		opts = {
+			style = "moon", -- Puedes probar: "storm", "night", "moon", "day"
+			transparent = true, -- Hace que el fondo sea transparente
+			terminal_colors = true, -- Aplica colores en la terminal
+		},
+	},
+}
